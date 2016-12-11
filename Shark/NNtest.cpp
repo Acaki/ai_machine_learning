f0354690bdbd8502932b1c1d6cba9a36503a6766
Acaki <axsz4298@gmail.com>
2016-12-12 00:03:17 +0800

    using two output neurons for better error rate, current error rate = 0.0866667

--- Shark/NNtest.cpp
+++ Shark/NNtest.cpp
@@ -17,8 +17,8 @@ template<class T>
 double experiment(AbstractStoppingCriterion<T>& stoppingCriterion, ClassificationDataset const& trainingset, ClassificationDataset const& testset){
 	FFNet<LogisticNeuron, LinearNeuron> network;
 	cout << "input dimension = " << inputDimension(trainingset) << endl;
-	cout << "number of classed = " << numberOfClasses(trainingset) << endl;
-	network.setStructure(inputDimension(trainingset), 10, numberOfClasses(trainingset) - 1);
+	cout << "number of classes = " << numberOfClasses(trainingset) << endl;
+	network.setStructure(inputDimension(trainingset), 10, numberOfClasses(trainingset));
 	initRandomUniform(network, -0.1, 0.1);
 
 	CrossEntropy loss;
@@ -29,7 +29,7 @@ double experiment(AbstractStoppingCriterion<T>& stoppingCriterion, Classificatio
 	OptimizationTrainer< FFNet<LogisticNeuron, LinearNeuron>, unsigned int > trainer(&loss, &optimizer, &stoppingCriterion);
 	trainer.train(network, trainingset);
 
-	ZeroOneLoss<unsigned int, RealVector> loss01(0.5);
+	ZeroOneLoss<unsigned int, RealVector> loss01;
 	Data<RealVector> predictions = network(testset.inputs());
 	cout << predictions << endl;
 	return loss01(testset.labels(), predictions);
@@ -40,22 +40,26 @@ int main( int argc, char ** argv )
 	ClassificationDataset data;
 	importCSV(data, argv[1], LAST_COLUMN, ',');
 	//data.shuffle();
+	cout << "number of elements = " << data.numberOfElements() << endl;
+	cout << "number of batches = " << data.numberOfBatches() << endl;
+
 	ClassificationDataset test = splitAtElement(data, static_cast<size_t>(0.75*data.numberOfElements()));
-	ClassificationDataset validation = splitAtElement(data,static_cast<std::size_t>(0.66*data.numberOfElements()));
+	//ClassificationDataset validation = splitAtElement(data,static_cast<std::size_t>(0.66*data.numberOfElements()));
 
 	//simple stopping criterion which allows for n iterations (here n = 10,100,500)
 	MaxIterations<> maxIterations(10);
-	double resultMaxIterations1 = experiment(maxIterations,data,test);
-	/*
+	//double resultMaxIterations1 = experiment(maxIterations,data,test);
+
 	maxIterations.setMaxIterations(100);
 	double resultMaxIterations2 = experiment(maxIterations,data,test);
-	maxIterations.setMaxIterations(500);
-	double resultMaxIterations3 = experiment(maxIterations,data,test);
+	//maxIterations.setMaxIterations(500);
+	//double resultMaxIterations3 = experiment(maxIterations,data,test);
 
-	TrainingError<> trainingError(10,1.e-5);
-	double resultTrainingError = experiment(trainingError,data,test);
+	//TrainingError<> trainingError(10,1.e-5);
+	//double resultTrainingError = experiment(trainingError,data,test);
 
 	//for the validated stopping criteria we need to define an error function using the validation set
+	/*
 	FFNet<LogisticNeuron,LogisticNeuron> network;
 	network.setStructure(inputDimension(data),10,numberOfClasses(data));
 	CrossEntropy loss;
@@ -69,11 +73,9 @@ int main( int argc, char ** argv )
 	//print the results
 	cout << "RESULTS: " << endl;
 	cout << "======== \n" << endl;
-	cout << "10 iterations   : " << resultMaxIterations1 << endl;
-	/*
+	//cout << "10 iterations   : " << resultMaxIterations1 << endl;
 	cout << "100 iterations : " << resultMaxIterations2 << endl;
-	cout << "500 iterations : " << resultMaxIterations3 << endl;
-	cout << "training Error : " << resultTrainingError << endl;
-	cout << "generalization Quotient : " << resultGeneralizationQuotient << endl;
-	*/
+	//cout << "500 iterations : " << resultMaxIterations3 << endl;
+	//cout << "training Error : " << resultTrainingError << endl;
+	//cout << "generalization Quotient : " << resultGeneralizationQuotient << endl;
 }
