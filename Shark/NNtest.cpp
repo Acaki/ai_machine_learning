06fce05cef5be50a03ccfec7b9149d935460ae6b
Acaki <axsz4298@gmail.com>
2016-12-12 03:13:51 +0800

    use ArgMaxConverter to convert two dimensional outputs to binary 0/1s

--- Shark/NNtest.cpp
+++ Shark/NNtest.cpp
@@ -16,11 +16,13 @@ using namespace std;
 
 template<class T>
 double experiment(AbstractStoppingCriterion<T>& stoppingCriterion, ClassificationDataset const& trainingset, ClassificationDataset const& testset){
-	FFNet<LogisticNeuron, LinearNeuron> network;
+	//use ArgMaxConverter to convert the output vector to binary 0/1
+	ArgMaxConverter<FFNet<LogisticNeuron, LinearNeuron> > network;
 	cout << "input dimension = " << inputDimension(trainingset) << endl;
 	cout << "number of classes = " << numberOfClasses(trainingset) << endl;
-	network.setStructure(inputDimension(trainingset), 10, numberOfClasses(trainingset) - 1);
-	initRandomUniform(network, -0.1, 0.1);
+	//create a feed forward neural network with one layer of 10 hidden neurons and one output for every class
+	network.decisionFunction().setStructure(inputDimension(trainingset), 10, numberOfClasses(trainingset));
+	initRandomUniform(network.decisionFunction(), -0.1, 0.1);
 
 	CrossEntropy loss;
 	//Improved Resilient-Backpropagation-algorithm with weight-backtracking
@@ -28,18 +30,12 @@ double experiment(AbstractStoppingCriterion<T>& stoppingCriterion, Classificatio
 
 	//Constructor: shark::OptimizationTrainer< Model, LabelTypeT >::OptimizationTrainer	(	LossType * 	loss, OptimizerType * 	optimizer, StoppingCriterionType * 	stoppingCriterion )
 	OptimizationTrainer< FFNet<LogisticNeuron, LinearNeuron>, unsigned int > trainer(&loss, &optimizer, &stoppingCriterion);
-	trainer.train(network, trainingset);
+	trainer.train(network.decisionFunction(), trainingset);
 
-	ZeroOneLoss<unsigned int, RealVector> loss01;
-	Data<RealVector> predictions = network(testset.inputs());
-	//cout << predictions << endl;
-	/*
-	ArgMaxConverter<Model> converter;
-	Data<unsigned int> outputClasses = converter(predictions);
-	cout << outputClasses << endl;
-	*/
-	ThresholdConverter converter;
-	cout << converter(predictions) << endl;
+	//default output type of ZeroOneLoss is unsigned int
+	ZeroOneLoss<> loss01;
+	Data<unsigned int> predictions = network(testset.inputs());
+	cout << predictions << endl;
 
 	return loss01(testset.labels(), predictions);
 }
@@ -52,8 +48,8 @@ int main( int argc, char ** argv )
 	cout << "number of elements = " << data.numberOfElements() << endl;
 	cout << "number of batches = " << data.numberOfBatches() << endl;
 
-	ClassificationDataset test = splitAtElement(data, static_cast<size_t>(0.995*data.numberOfElements()));
-	cout << test << endl;
+	ClassificationDataset test = splitAtElement(data, static_cast<size_t>(0.75*data.numberOfElements()));
+	//cout << test << endl;
 	//ClassificationDataset validation = splitAtElement(data,static_cast<std::size_t>(0.66*data.numberOfElements()));
 
 	//simple stopping criterion which allows for n iterations (here n = 10,100,500)
