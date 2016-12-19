70ab0b152ddd6e0869a024b032595d0598f9e699
Acaki <axsz4298@gmail.com>
2016-12-19 21:38:01 +0800

    Add random forest method, error = 0.052

---
+++ Shark/MLMethod.cpp
@@ -0,0 +1,131 @@
+#include <shark/Data/Csv.h>
+#include <shark/Data/Dataset.h>
+#include <shark/Models/Converter.h>
+#include <shark/Models/FFNet.h> //Feed forward neural network class
+#include <shark/Algorithms/GradientDescent/Rprop.h> //Optimization algorithm
+#include <shark/Algorithms/Trainers/CARTTrainer.h>     // the CART trainer
+#include <shark/Algorithms/Trainers/RFTrainer.h> //the random forest trainer
+#include <shark/ObjectiveFunctions/Loss/CrossEntropy.h> //Loss used for training
+#include <shark/ObjectiveFunctions/Loss/ZeroOneLoss.h> //The real loss for testing.
+#include <shark/ObjectiveFunctions/Loss/SquaredHingeLoss.h>
+#include <shark/Algorithms/Trainers/OptimizationTrainer.h> // Trainer wrapping iterative optimization
+#include <shark/Algorithms/StoppingCriteria/MaxIterations.h> //A simple stopping criterion that stops after a fixed number of iterations
+#include <shark/Algorithms/StoppingCriteria/TrainingError.h> //Stops when the algorithm seems to converge
+#include <shark/Algorithms/StoppingCriteria/GeneralizationQuotient.h> //Uses the validation error to track the progress
+#include <shark/Algorithms/StoppingCriteria/ValidatedStoppingCriterion.h> //Adds the validation error to the value of the point
+#include <iostream>
+
+using namespace shark;
+using namespace std;
+
+template<class ModelType>
+class MLMethod{
+public:
+	MLMethod();
+	void trainFeedForwardNN(ClassificationDataset const& trainingset);
+	void testFeedForwardNN(UnlabeledData<RealVector> const& testset);
+	double evaluateLoss(Data<unsigned int> testLabel);
+	void trainCART(ClassificationDataset const& trainingset);
+	void testCART(UnlabeledData<RealVector> const& testset);
+	void trainRF(ClassificationDataset const& trainingset);
+	void testRF(UnlabeledData<RealVector> const& testset);
+	//Output the predictions with generated hypothesis.
+	template<class T>
+	friend ostream& operator <<(ostream& output, const MLMethod<T>& method);
+
+private:
+	ModelType m_model;
+	double m_error;
+	Data<unsigned int> m_predictions;
+};
+
+int main()
+{
+	ClassificationDataset trainingset, testset;
+	importCSV(trainingset, "data/traindata.csv", LAST_COLUMN);
+	importCSV(testset, "data/testdata.csv", LAST_COLUMN);
+
+	//ClassificationDataset validation = splitAtElement(data,static_cast<std::size_t>(0.66*data.numberOfElements()));
+	MLMethod<FFNet<FastSigmoidNeuron, LinearNeuron> > FFNN;
+	FFNN.trainFeedForwardNN(trainingset);
+	FFNN.testFeedForwardNN(testset.inputs());
+	//cout << FFNN << endl;
+	cout << "FFNN error = " << FFNN.evaluateLoss(testset.labels()) << endl;
+
+	MLMethod<CARTClassifier<RealVector> > CART;
+	CART.trainCART(trainingset);
+	CART.testCART(testset.inputs());
+	//cout << CART << endl;
+	cout << "CART error = " << CART.evaluateLoss(testset.labels()) << endl;
+
+	MLMethod<RFClassifier> RF;
+	RF.trainRF(trainingset);
+	RF.testRF(testset.inputs());
+	//cout << RF << endl;
+	cout << "RF error = " << RF.evaluateLoss(testset.labels()) << endl;
+}
+
+template<class ModelType>
+MLMethod<ModelType>::MLMethod(){
+	m_error = 1.0;
+}
+
+template<class ModelType>
+void MLMethod<ModelType>::trainFeedForwardNN(ClassificationDataset const& trainingset){
+
+	//create a feed forward neural network with one layer of 10 hidden neurons and one output for every class
+	m_model.setStructure(inputDimension(trainingset), 10, numberOfClasses(trainingset));
+	initRandomUniform(m_model, -0.1, 0.1);
+
+	SquaredHingeLoss loss;
+	//Improved Resilient-Backpropagation-algorithm with weight-backtracking
+	IRpropPlus optimizer;
+	MaxIterations<> maxIterations(100);
+
+	//Constructor: shark::OptimizationTrainer< Model, LabelTypeT >::OptimizationTrainer	(	LossType * 	loss, OptimizerType * 	optimizer, StoppingCriterionType * 	stoppingCriterion )
+	OptimizationTrainer<FFNet<FastSigmoidNeuron, LinearNeuron>, unsigned int> trainer(&loss, &optimizer, &maxIterations);
+	trainer.train(m_model, trainingset);
+}
+
+template<class ModelType>
+void MLMethod<ModelType>::testFeedForwardNN(UnlabeledData<RealVector> const& testset){
+	ArgMaxConverter<ModelType> network(m_model);
+	m_predictions = network(testset);
+}
+
+template<class ModelType>
+double MLMethod<ModelType>::evaluateLoss(Data<unsigned int> testLabel){
+	ZeroOneLoss<> loss;
+	m_error = loss(testLabel, m_predictions);
+	return m_error;
+}
+
+template<class ModelType>
+void MLMethod<ModelType>::trainCART(ClassificationDataset const& trainingset){
+	CARTTrainer trainer;
+	trainer.train(m_model, trainingset);
+}
+
+template<class ModelType>
+void MLMethod<ModelType>::testCART(UnlabeledData<RealVector> const& testset){
+	ArgMaxConverter<ModelType> tree(m_model);
+	m_predictions = tree(testset);
+}
+
+template<class ModelType>
+void MLMethod<ModelType>::trainRF(ClassificationDataset const& trainingset){
+	RFTrainer trainer;
+	trainer.train(m_model, trainingset);
+}
+
+template<class ModelType>
+void MLMethod<ModelType>::testRF(UnlabeledData<RealVector> const& testset){
+	ArgMaxConverter<ModelType> tree(m_model);
+	m_predictions = tree(testset);
+}
+
+template<class T>
+ostream& operator <<(ostream& output, const MLMethod<T>& method){
+	output << method.m_predictions;
+	return output;
+}
