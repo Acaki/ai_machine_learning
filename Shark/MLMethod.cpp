62e409c5b49529fa7d8503836c6e2ec525da634d
acaki <axsz4298@gmail.com>
2016-12-26 00:55:49 +0800

    Adjust ensemble method - 3

--- Shark/MLMethod.cpp
+++ Shark/MLMethod.cpp
@@ -180,8 +180,13 @@ void FeedForwardNeuralNetwork::trainFeedForwardNN(ClassificationDataset const& t
 }
 
 void FeedForwardNeuralNetwork::ensemble(ClassificationDataset const& trainingset, ClassificationDataset const& testset, UnlabeledData<RealVector> unlabeledset){
-	size_t predictSize = testset.numberOfElements();
-	vector<double> vote(predictSize, 0);
+	size_t testSize = testset.numberOfElements();
+	size_t unlabeledSize = unlabeledset.numberOfElements();
+	//Vector for storing weighted votes for testset
+	vector<double> testVote(testSize, 0);
+	//Vector for storing weighted votes for unlabeledset
+	vector<double> unlabeledVote(unlabeledSize, 0);
+
 	double totalWeight = 0;
 	for (size_t i = 0; i < indivs; ++i){
 		FeedForwardNeuralNetwork individual(trainingset);
@@ -190,23 +195,31 @@ void FeedForwardNeuralNetwork::ensemble(ClassificationDataset const& trainingset
 		individual.test(testset.inputs());
 		double indivLoss = individual.evaluateLoss(testset.labels());
 		double indivWeight = 1 - indivLoss;
+		vector<double> weightedVote = individual.calWeightedVote();
+		transform(testVote.begin(), testVote.end(), weightedVote.begin(), testVote.begin(), plus<double>());
 
 		//Now generate the predictions according to unlabeledset.
 		individual.test(unlabeledset);
-		vector<double> weightedVote = individual.calWeightedVote();
+		weightedVote = individual.calWeightedVote();
+		transform(vote.begin(), vote.end(), weightedVote.begin(), vote.begin(), plus<double>());
 
 		totalWeight += indivWeight;
 		cout << "weight " << i << " = " << indivWeight << endl;
-		//Accumulate the weighted vote
-		transform(vote.begin(), vote.end(), weightedVote.begin(), vote.begin(), plus<double>());
 	}
 	//Determine majority
-	for (auto &pos: vote){
+	for (auto &pos: testVote){
 		pos = (totalWeight - pos) >= (pos - 0) ? 0 : 1;
 	}
+	for (auto &pos: unlabeledVote){
+		pos = (totalWeight - pos) >= (pos - 0) ? 0 : 1;
+	}
+	//Evaluate error rate for testset
+	vector<unsigned int> finalPredictions(testVote.begin(), testVote.end());
+	m_predictions = createDataFromRange(finalPredictions);
+	evaluateLoss(testset.labels());
 
-	vector<unsigned int> finalPredictions(vote.begin(), vote.end());
 	//Assign predictions for unlabeledset
+	finalPredictions = vector<unsigned int>(unlabeledVote.begin(), unlabeledVote.end());
 	m_predictions = createDataFromRange(finalPredictions);
 }
 
