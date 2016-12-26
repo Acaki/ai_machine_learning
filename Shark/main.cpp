1a5ffbe0087add123cbc8885415f862a15567b52
acaki <axsz4298@gmail.com>
2016-12-27 03:28:52 +0800

    Add output function
    The built-in exportCSV in Shark seems to be broken,
    it failed to export any type of Data<unsigned int>.

--- Shark/main.cpp
+++ Shark/main.cpp
@@ -1,25 +1,30 @@
+#include <fstream>
 #include "Hypothesis.h"
 #include "SupervisedMethod.h"
 
-Data<unsigned int> ensemble(ClassificationDataset const& trainingset, ClassificationDataset const& testset, UnlabeledData<RealVector> const& unlabeledset);
+vector<unsigned int> ensemble(ClassificationDataset const& trainingset, ClassificationDataset const& testset, UnlabeledData<RealVector> const& unlabeledset);
+void outputToFile(vector<unsigned int> const& output);
 
 int main(){
 	ClassificationDataset trainingset, testset;
 	UnlabeledData<RealVector> unlabeledset;
 
 	importCSV(trainingset, "data/Tradata.csv", LAST_COLUMN);
+	cout << "Loading training set from data/Tradata.csv - ok" << endl;
 	importCSV(unlabeledset, "data/input.csv");
+	cout << "Loading unlabeled set from data/input.csv - ok" << endl;
 	trainingset.shuffle();
 	testset = splitAtElement(trainingset, static_cast<size_t>(0.8*trainingset.numberOfElements()));
 
-	Data<unsigned int> predictions = ensemble(trainingset, testset, unlabeledset);
-	exportCSV(predictions, "data/output.csv");
-	cout << predictions  << endl;
+	cout << "Now generating predictions for unlabeled set ..." << endl;
+	vector<unsigned int> predictions = ensemble(trainingset, testset, unlabeledset);
+	outputToFile(predictions);
+	cout << "Complete, the predictions are written into data/output.csv" << endl;
 
 	return 0;
 }
 
-Data<unsigned int> ensemble(ClassificationDataset const& trainingset, ClassificationDataset const& testset, UnlabeledData<RealVector> const& unlabeledset){
+vector<unsigned int> ensemble(ClassificationDataset const& trainingset, ClassificationDataset const& testset, UnlabeledData<RealVector> const& unlabeledset){
   FeedForwardNeuralNetwork FFNN(trainingset);
   //Generate the final predictions for the unlabeledset
   FFNN.ensemble(trainingset, testset, unlabeledset);
@@ -46,5 +51,13 @@ Data<unsigned int> ensemble(ClassificationDataset const& trainingset, Classifica
     pos = (totalWeight - pos) >= (pos - 0) ? 0 : 1;
   }
   vector<unsigned int> finalPredictions(vote.begin(), vote.end());
-	return createDataFromRange(finalPredictions);
+	return finalPredictions;
+}
+
+void outputToFile(vector<unsigned int> const& output){
+	ofstream outFile;
+	outFile.open("data/output.csv");
+	for (auto const& pos: output){
+		outFile << pos << endl;
+	}
 }
